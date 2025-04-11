b18cc0166a0867efb5c250bcb580ab2bbeb2f365
Fuji <fujiji.entire1414@gmail.com>
2025-04-11 21:07:12 +0900

    Judge Max, Min, Sum

--- main.go
+++ main.go
@@ -1,14 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
-	var i int
-	fmt.Scan(&i)
+	var n int
+	fmt.Scan(&n)
 
-	if i%2 == 0 {
-		fmt.Printf("%d is Even", i)
-	} else {
-		fmt.Printf("%d is Odd", i)
+	minNum := math.MaxInt64
+	maxNum := math.MinInt64
+	sum := 0
+
+	for i := 0; i < n; i++ {
+		var a int
+		fmt.Scan(&a)
+
+		if a > maxNum {
+			maxNum = a
+		}
+		if a < minNum {
+			minNum = a
+		}
+		sum += a
 	}
+	fmt.Printf("Max: %d Min: %d Sum: %d", maxNum, minNum, sum)
 }
