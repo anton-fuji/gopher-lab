0349bd69d262f15699284c91990d242b6d5982e0
Fuji <fujiji.entire1414@gmail.com>
2025-04-11 20:56:25 +0900

    偶奇判定_解答

--- fuji.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "fmt"
-
-func Fuji(a, b int) {
-	fmt.Println(a + b)
-}
--- main.go
+++ main.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var i int
+	fmt.Scan(&i)
+
+	if i%2 == 0 {
+		fmt.Printf("%d is Even", i)
+	} else {
+		fmt.Printf("%d is Odd", i)
+	}
 }
