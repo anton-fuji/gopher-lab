98bf9a0a61e930a335357bac5936d8b7ba600e5d
Fuji <fujiji.entire1414@gmail.com>
2025-06-10 16:21:41 +0900

    goでcat コマンドの実装

--- main.go
+++ main.go
@@ -1,25 +1,57 @@
 package main
 
 import (
+	"bufio"
+	"flag"
 	"fmt"
+	"os"
 )
 
 func main() {
-	var a, b int
-	var op string
-	fmt.Scan(&a, &op, &b)
+	showLineNum := flag.Bool("n", false, "show line numbers")
+	flag.Parse()
 
-	if op == "?" {
-		fmt.Println("変なの入れるな!")
+	// 残りのファイルパスを取得
+	files := flag.Args()
+
+	// 指定なしの場合はエラーを出力
+	if len(files) == 0 {
+		fmt.Fprintf(os.Stderr, "Usage: %s [-n] file1 [file2 ...]\n", os.Args[0])
+		os.Exit(1)
 	}
-	switch {
-	case op == "+":
-		fmt.Println(a + b)
-	case op == "-":
-		fmt.Println(a - b)
-	case op == "*":
-		fmt.Println(a * b)
-	case op == "/":
-		fmt.Println(a / b)
+
+	lineNumber := 1
+
+	for _, filename := range files {
+		err := processFile(filename, *showLineNum, &lineNumber)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error processing %s: %v\n", filename, err)
+			os.Exit(1)
+		}
 	}
+}
+
+func processFile(filename string, showLineNumber bool, lineNumber *int) error {
+	file, err := os.Open(filename)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		if showLineNumber {
+			fmt.Printf("%6d\t%s\n", *lineNumber, line)
+		} else {
+			fmt.Println(line)
+		}
+		(*lineNumber)++
+	}
+
+	if err := scanner.Err(); err != nil {
+		return err
+	}
+	return nil
 }
