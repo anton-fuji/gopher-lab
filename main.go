5947a6d68450269a384391d3e7736c19281a8baf
Fuji <fujiji.entire1414@gmail.com>
2025-06-14 09:18:01 +0900

    bufioで高速化

--- main.go
+++ main.go
@@ -2,56 +2,15 @@ package main
 
 import (
 	"bufio"
-	"flag"
 	"fmt"
 	"os"
 )
 
 func main() {
-	showLineNum := flag.Bool("n", false, "show line numbers")
-	flag.Parse()
+	var s string
+	sc := bufio.NewReader(os.Stdin)
+	fmt.Fscan(sc, &s)
 
-	// 残りのファイルパスを取得
-	files := flag.Args()
+	fmt.Println(s)
 
-	// 指定なしの場合はエラーを出力
-	if len(files) == 0 {
-		fmt.Fprintf(os.Stderr, "Usage: %s [-n] file1 [file2 ...]\n", os.Args[0])
-		os.Exit(1)
-	}
-
-	lineNumber := 1
-
-	for _, filename := range files {
-		err := processFile(filename, *showLineNum, &lineNumber)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "Error processing %s: %v\n", filename, err)
-			os.Exit(1)
-		}
-	}
-}
-
-func processFile(filename string, showLineNumber bool, lineNumber *int) error {
-	file, err := os.Open(filename)
-	if err != nil {
-		return err
-	}
-	defer file.Close()
-
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		line := scanner.Text()
-
-		if showLineNumber {
-			fmt.Printf("%6d\t%s\n", *lineNumber, line)
-		} else {
-			fmt.Println(line)
-		}
-		(*lineNumber)++
-	}
-
-	if err := scanner.Err(); err != nil {
-		return err
-	}
-	return nil
 }
