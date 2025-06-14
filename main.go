ecd28bd60b1dbfae87240b2cc192c0e736bbb339
Fuji <fujiji.entire1414@gmail.com>
2025-06-14 17:45:07 +0900

    HTTPサーバをnet/httpパッケージで立てる

--- main.go
+++ main.go
@@ -1,19 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
-import "time"
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello, HTTPサーバー")
+}
 
 func main() {
-	ch := make(chan int) // 容量 0
-	go func() {          // goroutine 1
-		ch <- 100
-	}()
-
-	go func() { // goroutine 2
-		v := <-ch
-		fmt.Println(v)
-	}()
-
-	time.Sleep(2 * time.Second)
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
 }
