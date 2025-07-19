fe2f3db30a918e6e51acb64b2333f5fac000ebef
Fuji <fujiji.entire1414@gmail.com>
2025-07-19 22:26:00 +0900

    Webサーバーの実装

--- sample-server.go
+++ sample-server.go
@@ -3,65 +3,93 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
-	"strconv"
-	"strings"
 )
 
-type Todo struct {
-	ID   int    `json:"id"`
-	Task string `json:"task"`
+// ハンドラ
+type helloHandler struct{}
+
+func (hh *helloHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	name := r.PathValue("name")
+	fmt.Fprintf(w, "Hello!, %q\n", name)
 }
 
-var todos []Todo
-var nextID = 1
+// ハンドラ
+type byeHandler struct{}
+
+func (bh *byeHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	name := r.PathValue("name")
+	fmt.Fprintf(w, "Bye!, %q\n", name)
+}
+
+// ハンドラ
+type customGreetHandler struct {
+	Greet string `json:"greet"`
+	Name  string `json:"name"`
+}
+
+func (cgh *customGreetHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if err := json.NewDecoder(r.Body).Decode(&cgh); err != nil {
+		fmt.Fprintf(w, "decode err: %v", err)
+	}
+
+	fmt.Fprintf(w, "%v!, %q\n", cgh.Greet, cgh.Name)
+}
+
+// ハンドラ
+func fetchUser(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Fuji")
+}
+
+func addUser(w http.ResponseWriter, r *http.Request) {
+	type userData struct {
+		ID   int    `json:"id"`
+		Name string `json:"name"`
+	}
+	var user userData
+	if err := json.NewDecoder(r.Body).Decode(&user); err != nil {
+		fmt.Fprintf(w, "decode err: %v", err)
+	}
+	fmt.Fprintf(w, "user was added, id : %d, name: %q", user.ID, user.Name)
+}
+
+// ロガーミドルウェア
+func loggingMidlleware(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		method := r.Method
+		path := r.URL.Path
+		log.Printf("%v => %v", method, path)
+		h.ServeHTTP(w, r)
+	})
+}
+
+// ルーティングを初期化する
+func initRoutes() *http.ServeMux {
+	mux := http.NewServeMux()
+	{
+		greetMux := http.NewServeMux()
+		greetMux.Handle("GET /greet/hello/{name...}", loggingMidlleware(&helloHandler{}))
+		greetMux.Handle("GET /greet/bye/{name...}", loggingMidlleware(&byeHandler{}))
+		greetMux.Handle("POST /greet/custom", loggingMidlleware(&customGreetHandler{}))
+
+		mux.Handle("/greet/", greetMux)
+	}
+
+	{
+		userMux := http.NewServeMux()
+		userMux.Handle("GET /user/{id}", loggingMidlleware(http.HandlerFunc(fetchUser)))
+		userMux.Handle("POST /user/{id}", loggingMidlleware(http.HandlerFunc(addUser)))
+
+		mux.Handle("/user/", userMux)
+	}
+
+	return mux
+}
 
 func main() {
-	http.HandleFunc("/hello", helloHandler)
-	http.HandleFunc("/todos", todosHnadler)
-	http.HandleFunc("/todos/", todoByIdHnadler)
+	mux := initRoutes()
 
-	fmt.Println("Server running at: 8000")
-	http.ListenAndServe(":8000", nil)
-}
-
-func helloHandler(w http.ResponseWriter, r *http.Request) {
-	json.NewEncoder(w).Encode(map[string]string{"message": "Hello, World!"})
-}
-
-func todosHnadler(w http.ResponseWriter, r *http.Request) {
-	if r.Method == "GET" {
-		json.NewEncoder(w).Encode(todos)
-		return
-	}
-	if r.Method == "POST" {
-		var t Todo
-		if err := json.NewDecoder(r.Body).Decode(&t); err != nil {
-			http.Error(w, "Invalid", http.StatusBadRequest)
-			return
-		}
-
-		t.ID = nextID
-		nextID++
-		todos = append(todos, t)
-		json.NewEncoder(w).Encode(t)
-		return
-	}
-	http.Error(w, "Method Not Allow", http.StatusMethodNotAllowed)
-}
-
-func todoByIdHnadler(w http.ResponseWriter, r *http.Request) {
-	idStr := strings.TrimPrefix(r.URL.Path, "/todos/")
-	id, err := strconv.Atoi(idStr)
-	if err != nil {
-		http.Error(w, "Invalid ID", http.StatusBadRequest)
-		return
-	}
-	for _, t := range todos {
-		if t.ID == id {
-			json.NewEncoder(w).Encode(t)
-			return
-		}
-	}
-	http.Error(w, "Not Found", http.StatusBadRequest)
+	fmt.Println("Running Server at 8000")
+	http.ListenAndServe(":8000", mux)
 }
