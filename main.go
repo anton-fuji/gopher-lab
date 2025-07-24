42d8c62d42168f31b546586bcac7abe04ebda6c1
Fuji <fujiji.entire1414@gmail.com>
2025-07-24 18:43:52 +0900

    Go-Alog復習

--- main.go
+++ main.go
@@ -2,24 +2,20 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 )
 
-func uniques[T comparable](s []T) []T {
-	count := map[T]struct{}{}
-	result := []T{}
-
-	for _, v := range s {
-		if _, ok := count[v]; !ok {
-			count[v] = struct{}{}
-			result = append(result, v)
-		}
-	}
-	return result
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
 }
-func main() {
-	s := []string{"apple", "banana", "oarange", "apple", "peach"}
-	i := []int{1, 2, 4, 2, 4, 6, 2, 8, 3}
 
-	fmt.Println(uniques(s))
-	fmt.Println(uniques(i))
+func hiHandler(w http.ResponseWriter, r *http.Request) {
+	name := r.PathValue("name")
+	fmt.Fprintf(w, "Hi, %q!\n", name)
+}
+
+func main() {
+	http.HandleFunc("/", helloHandler)
+	http.HandleFunc("/hello", hiHandler)
+	http.ListenAndServe(":8000", nil)
 }
